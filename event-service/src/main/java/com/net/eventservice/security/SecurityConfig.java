8ccb916fcd5242a3195fb0cd39d2d5b04d73cefc
MohammedFadliMF <fadlimohamed155@gmail.com>
2026-02-01 00:50:12 +0100

    c

--- event-service/src/main/java/com/net/eventservice/security/SecurityConfig.java
+++ event-service/src/main/java/com/net/eventservice/security/SecurityConfig.java
@@ -32,10 +32,7 @@ public class SecurityConfig {
                                 .sessionManagement(session -> session
                                                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                                 .authorizeHttpRequests(ar -> ar
-<<<<<<< HEAD
-=======
                                                 .requestMatchers("/api/events/auth").permitAll()
->>>>>>> dev
                                                 .requestMatchers("/actuator/**").permitAll()
                                                 .requestMatchers("/actuator/health/**").permitAll()
                                                 .requestMatchers(
